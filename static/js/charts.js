959f4d24b950fcf7192e0cfc34793a700c9c4c7f
Noor Sami <noor.u.sami@gmail.com>
2022-09-21 19:10:24 -0400

    Updating chart function.

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("../data/samples.json").then((data) => {
+  d3.json("C:/Users/nooru/Downloads/Bellybutton_Diversity/data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("../data/samples.json").then((data) => {
+  d3.json("C:/Users/nooru/Downloads/Bellybutton_Diversity/data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,14 +56,15 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("../data/samples.json").then((data) => {
+  d3.json("C:/Users/nooru/Downloads/Bellybutton_Diversity/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-      var samples = data.samples;
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var filteredSample = samples.filter(obj => obj.id == sample);
-
+    console.log(sampleResult)
     //  5. Create a variable that holds the first sample in the array.
     var first = filteredSample.slice(0);
+    console.log(filteredSample)
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = filteredSample.otu_ids;
@@ -98,21 +99,8 @@ function buildCharts(sample) {
       }
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("plot", data, layout);
-  });
-}
+    Plotly.newPlot("bar", barData, barLayout);
 
-// Bubble Chart 
-// Create the buildCharts function.
-function buildCharts(sample) {
-  // Use d3.json to load and retrieve the samples.json file 
-  d3.json("../data/samples.json").then((data) => {
-    
-
-    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("plot", data, layout); 
-
-    // 1. Create the trace for the bubble chart.
     var bubbleData = {
       x: sample_values.slice[0,10],
       y: yticks,
@@ -134,15 +122,7 @@ function buildCharts(sample) {
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("plot", bubbleData, bubbleLayout); 
-  });
-}
-
-// Create the buildChart function.
-function buildCharts(sample) {
-  // Use d3.json to load the samples.json file 
-  d3.json("../data/samples.json").then((data) => {
-    console.log(data);
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metadata = data.metadata;
@@ -151,15 +131,8 @@ function buildCharts(sample) {
     // 2. Create a variable that holds the first sample in the metadata array.
     var firstMeta = metadataArray.slice(0);
 
-
     // 3. Create a variable that holds the washing frequency.
     var washfreq = metadataArray.wfreq;
-
-    // Use Plotly to plot the bar data and layout.
-    Plotly.newPlot("plot", data, layout);
-    
-    // Use Plotly to plot the bubble data and layout.
-    Plotly.newPlot("plot", bubbleData, bubbleLayout);
   
     
     // 4. Create the trace for the gauge chart.
@@ -172,10 +145,10 @@ function buildCharts(sample) {
         axis: {range: [null, 10], dtick: "2"},
         bar: {color: "black"},
         steps: [
-          {range: [0,2], color: "red"}
-          {range: [2,4], color: "orange"}
-          {range: [4,6], color: "yellow"}
-          {range: [6,8], color: "green"}
+          {range: [0,2], color: "red"},
+          {range: [2,4], color: "orange"},
+          {range: [4,6], color: "yellow"},
+          {range: [6,8], color: "green"},
           {range: [8,10], color: "blue"}
         ]
       }
@@ -184,10 +157,17 @@ function buildCharts(sample) {
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-     automargin: true
+     width: 500,
+     height: 500,
+     margin: {
+      t: 0,
+      b: 0
+     }
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
     Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+
   });
 }
+
